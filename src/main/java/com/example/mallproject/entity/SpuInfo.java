3e7d7933baa458f637eb8fa2c55f23c0de8223e6
zzzk1 <36791902+zzzk1@users.noreply.github.com>
2023-04-16 14:11:31 +0800

    sku spu

---
+++ src/main/java/com/example/mallproject/entity/SpuInfo.java
@@ -0,0 +1,67 @@
+package com.example.mallproject.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * spu信息
+ * </p>
+ *
+ * @author zzzk1
+ * @since 2023-04-16
+ */
+@Getter
+@Setter
+@Accessors(chain = true)
+@TableName("pms_spu_info")
+public class SpuInfo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 商品id
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 商品名称
+     */
+    private String spuName;
+
+    /**
+     * 商品描述
+     */
+    private String spuDescription;
+
+    /**
+     * 所属分类id
+     */
+    private Long catalogId;
+
+    /**
+     * 品牌id
+     */
+    private Long brandId;
+
+    private BigDecimal weight;
+
+    /**
+     * 上架状态[0 - 下架，1 - 上架]
+     */
+    private Integer publishStatus;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
+
+
+}
---
+++ src/main/java/com/example/mallproject/service/impl/SpuInfoServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.mallproject.service.impl;
+
+import com.example.mallproject.entity.SpuInfo;
+import com.example.mallproject.mapper.SpuInfoMapper;
+import com.example.mallproject.service.SpuInfoService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * spu信息 服务实现类
+ * </p>
+ *
+ * @author zzzk1
+ * @since 2023-04-16
+ */
+@Service
+public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoMapper, SpuInfo> implements SpuInfoService {
+
+}
