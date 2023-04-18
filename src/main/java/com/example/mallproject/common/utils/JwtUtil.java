be4993e600a266cafabe30649b46f8ac118d8b31
zzzk1 <36791902+zzzk1@users.noreply.github.com>
2023-04-18 18:00:57 +0800

    完成权限管理

---
+++ src/main/java/com/example/mallproject/common/utils/JwtUtil.java
@@ -0,0 +1,66 @@
+package com.example.mallproject.common.utils;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.mallproject.entity.User;
+import com.example.mallproject.service.UserService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+/**
+ * @author zzzk1
+ */
+public class JwtUtil {
+
+    // Token过期时间30分钟
+    public static final long EXPIRE_TIME = 30 * 60 * 1000;
+
+    public static boolean verify(String token, String username, String secret) {
+        try {
+            // 设置加密算法
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withClaim("username", username)
+                    .build();
+            // 效验TOKEN
+            DecodedJWT jwt = verifier.verify(token);
+            return true;
+        } catch (Exception exception) {
+            return false;
+        }
+    }
+
+
+
+
+    public static String sign(String username, String secret) {
+        Date date = new Date(System.currentTimeMillis() + EXPIRE_TIME);
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        // 附带username信息
+        return JWT.create()
+                .withClaim("username", username)
+                .withExpiresAt(date)
+                .sign(algorithm);
+
+    }
+
+
+    public static String getUserNameByToken(HttpServletRequest request)  {
+        String token = request.getHeader("token");
+        DecodedJWT jwt = JWT.decode(token);
+        return jwt.getClaim("username")
+                .asString();
+    }
+
+
+
+}
--- src/main/java/com/example/mallproject/config/SecurityInterceptor.java
+++ src/main/java/com/example/mallproject/config/SecurityInterceptor.java
@@ -1,153 +1,153 @@
-package com.example.mallproject.config;
-
-import com.example.mallproject.Annotation.LoginRequired;
-import com.example.mallproject.Annotation.PermissionRequired;
-import com.example.mallproject.common.Exception.BizException;
-import com.example.mallproject.common.api.Logical;
-import com.example.mallproject.common.api.ResultCode;
-import com.example.mallproject.common.api.UserType;
-import com.example.mallproject.common.api.WebConstant;
-import com.example.mallproject.common.utils.ThreadLocalUtil;
-import com.example.mallproject.entity.User;
-import org.springframework.core.annotation.AnnotationUtils;
-import org.springframework.web.method.HandlerMethod;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
-
-public class SecurityInterceptor extends HandlerInterceptorAdapter {
-
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
-        // 如果方法上没有加@LoginRequired或@PermissionRequired（上面叠加了@LoginRequired），直接放行
-        if (isLoginFree(handler)) {
-            return true;
-        }
-
-        // 登录校验
-        User user = handleLogin(request, response);
-        ThreadLocalUtil.put(WebConstant.USER_INFO, user);
-
-        // 权限校验
-        checkPermission(user, handler);
-
-        // 放行到Controller
-        return super.preHandle(request, response, handler);
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        // 及时移除，避免ThreadLocal内存泄漏
-        ThreadLocalUtil.remove(WebConstant.USER_INFO);
-        super.afterCompletion(request, response, handler, ex);
-    }
-
-    /**
-     * 接口是否免登录（支持Controller上添加@LoginRequired）
-     *
-     * @param handler
-     * @return
-     */
-    private boolean isLoginFree(Object handler) {
-
-        // 判断是否支持免登录
-        if (handler instanceof HandlerMethod) {
-            HandlerMethod handlerMethod = (HandlerMethod) handler;
-
-            // 类上是否有@LoginRequired
-            Class<?> controllerClazz = handlerMethod.getBeanType();
-            LoginRequired ControllerLogin = AnnotationUtils.findAnnotation(controllerClazz, LoginRequired.class);
-
-            // 方法上是否有@LoginRequired
-            Method method = handlerMethod.getMethod();
-            LoginRequired methodLogin = AnnotationUtils.getAnnotation(method, LoginRequired.class);
-
-            return ControllerLogin == null && methodLogin == null;
-        }
-
-        return true;
-    }
-
-    /**
-     * 是否需要权限认证（支持Controller上添加@PermissionRequired）
-     *
-     * @param handler
-     * @return
-     */
-    private boolean isPermissionFree(Object handler) {
-        // 判断是否需要权限认证
-        if (handler instanceof HandlerMethod) {
-            HandlerMethod handlerMethod = (HandlerMethod) handler;
-            Class<?> controllerClazz = handlerMethod.getBeanType();
-            Method method = handlerMethod.getMethod();
-            PermissionRequired controllerPermission = AnnotationUtils.getAnnotation(controllerClazz, PermissionRequired.class);
-            PermissionRequired methodPermission = AnnotationUtils.getAnnotation(method, PermissionRequired.class);
-            // 没有加@PermissionRequired，不需要权限认证
-            return controllerPermission == null && methodPermission == null;
-        }
-
-        return true;
-    }
-
-    /**
-     * 登录校验
-     *
-     * @param request
-     * @param response
-     * @return
-     */
-    private User handleLogin(HttpServletRequest request, HttpServletResponse response) {
-        HttpSession session = request.getSession();
-        User currentUser = (User) session.getAttribute(WebConstant.CURRENT_USER_IN_SESSION);
-        if (currentUser == null) {
-            // 抛异常，请先登录
-            throw new BizException(ResultCode.UNAUTHORIZED);
-        }
-        return currentUser;
-    }
-
-    /**
-     * 权限校验
-     *
-     * @param user
-     * @param handler
-     */
-    private void checkPermission(User user, Object handler) {
-        // 如果方法上没有加@PermissionRequired，直接放行
-        if (isPermissionFree(handler)) {
-            return;
-        }
-
-        if (handler instanceof HandlerMethod) {
-            HandlerMethod handlerMethod = (HandlerMethod) handler;
-            Method method = handlerMethod.getMethod();
-            Class<?> controllerClazz = handlerMethod.getBeanType();
-
-            PermissionRequired controllerPermission = AnnotationUtils.findAnnotation(controllerClazz, PermissionRequired.class);
-            PermissionRequired methodPermission = AnnotationUtils.getAnnotation(method, PermissionRequired.class);
-            if (hasPermission(controllerPermission, user.getUserType()) && hasPermission(methodPermission, user.getUserType())) {
-                return;
-            }
-
-            // 代码走到这，说明权限不匹配
-            throw new BizException(ResultCode.FORBIDDEN);
-        }
-    }
-
-    private boolean hasPermission(Annotation permissionAnnotation, Integer typeOfUser) {
-        if (permissionAnnotation == null) {
-            return true;
-        }
-
-        UserType[] userTypes = (UserType[]) AnnotationUtils.getValue(permissionAnnotation, "userType");
-        Logical logical = (Logical) AnnotationUtils.getValue(permissionAnnotation, "logical");
-        // 我把权限判断的逻辑封装到UserType枚举类中复用
-        return UserType.hasPermission(userTypes, typeOfUser, logical);
-    }
-
-}
+//package com.example.mallproject.config;
+//
+//import com.example.mallproject.Annotation.LoginRequired;
+//import com.example.mallproject.Annotation.PermissionRequired;
+//import com.example.mallproject.common.Exception.BizException;
+//import com.example.mallproject.common.api.Logical;
+//import com.example.mallproject.common.api.ResultCode;
+//import com.example.mallproject.common.api.UserType;
+//import com.example.mallproject.common.api.WebConstant;
+//import com.example.mallproject.common.utils.ThreadLocalUtil;
+//import com.example.mallproject.entity.User;
+//import org.springframework.core.annotation.AnnotationUtils;
+//import org.springframework.web.method.HandlerMethod;
+//import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
+//import java.lang.annotation.Annotation;
+//import java.lang.reflect.Method;
+//
+//public class SecurityInterceptor extends HandlerInterceptorAdapter {
+//
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//
+//        // 如果方法上没有加@LoginRequired或@PermissionRequired（上面叠加了@LoginRequired），直接放行
+//        if (isLoginFree(handler)) {
+//            return true;
+//        }
+//
+//        // 登录校验
+//        User user = handleLogin(request, response);
+//        ThreadLocalUtil.put(WebConstant.USER_INFO, user);
+//
+//        // 权限校验
+//        checkPermission(user, handler);
+//
+//        // 放行到Controller
+//        return super.preHandle(request, response, handler);
+//    }
+//
+//    @Override
+//    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+//        // 及时移除，避免ThreadLocal内存泄漏
+//        ThreadLocalUtil.remove(WebConstant.USER_INFO);
+//        super.afterCompletion(request, response, handler, ex);
+//    }
+//
+//    /**
+//     * 接口是否免登录（支持Controller上添加@LoginRequired）
+//     *
+//     * @param handler
+//     * @return
+//     */
+//    private boolean isLoginFree(Object handler) {
+//
+//        // 判断是否支持免登录
+//        if (handler instanceof HandlerMethod) {
+//            HandlerMethod handlerMethod = (HandlerMethod) handler;
+//
+//            // 类上是否有@LoginRequired
+//            Class<?> controllerClazz = handlerMethod.getBeanType();
+//            LoginRequired ControllerLogin = AnnotationUtils.findAnnotation(controllerClazz, LoginRequired.class);
+//
+//            // 方法上是否有@LoginRequired
+//            Method method = handlerMethod.getMethod();
+//            LoginRequired methodLogin = AnnotationUtils.getAnnotation(method, LoginRequired.class);
+//
+//            return ControllerLogin == null && methodLogin == null;
+//        }
+//
+//        return true;
+//    }
+//
+//    /**
+//     * 是否需要权限认证（支持Controller上添加@PermissionRequired）
+//     *
+//     * @param handler
+//     * @return
+//     */
+//    private boolean isPermissionFree(Object handler) {
+//        // 判断是否需要权限认证
+//        if (handler instanceof HandlerMethod) {
+//            HandlerMethod handlerMethod = (HandlerMethod) handler;
+//            Class<?> controllerClazz = handlerMethod.getBeanType();
+//            Method method = handlerMethod.getMethod();
+//            PermissionRequired controllerPermission = AnnotationUtils.getAnnotation(controllerClazz, PermissionRequired.class);
+//            PermissionRequired methodPermission = AnnotationUtils.getAnnotation(method, PermissionRequired.class);
+//            // 没有加@PermissionRequired，不需要权限认证
+//            return controllerPermission == null && methodPermission == null;
+//        }
+//
+//        return true;
+//    }
+//
+//    /**
+//     * 登录校验
+//     *
+//     * @param request
+//     * @param response
+//     * @return
+//     */
+//    private User handleLogin(HttpServletRequest request, HttpServletResponse response) {
+//        HttpSession session = request.getSession();
+//        User currentUser = (User) session.getAttribute(WebConstant.CURRENT_USER_IN_SESSION);
+//        if (currentUser == null) {
+//            // 抛异常，请先登录
+//            throw new BizException(ResultCode.UNAUTHORIZED);
+//        }
+//        return currentUser;
+//    }
+//
+//    /**
+//     * 权限校验
+//     *
+//     * @param user
+//     * @param handler
+//     */
+//    private void checkPermission(User user, Object handler) {
+//        // 如果方法上没有加@PermissionRequired，直接放行
+//        if (isPermissionFree(handler)) {
+//            return;
+//        }
+//
+//        if (handler instanceof HandlerMethod) {
+//            HandlerMethod handlerMethod = (HandlerMethod) handler;
+//            Method method = handlerMethod.getMethod();
+//            Class<?> controllerClazz = handlerMethod.getBeanType();
+//
+//            PermissionRequired controllerPermission = AnnotationUtils.findAnnotation(controllerClazz, PermissionRequired.class);
+//            PermissionRequired methodPermission = AnnotationUtils.getAnnotation(method, PermissionRequired.class);
+//            if (hasPermission(controllerPermission, user.getUserType()) && hasPermission(methodPermission, user.getUserType())) {
+//                return;
+//            }
+//
+//            // 代码走到这，说明权限不匹配
+//            throw new BizException(ResultCode.FORBIDDEN);
+//        }
+//    }
+//
+//    private boolean hasPermission(Annotation permissionAnnotation, Integer typeOfUser) {
+//        if (permissionAnnotation == null) {
+//            return true;
+//        }
+//
+//        UserType[] userTypes = (UserType[]) AnnotationUtils.getValue(permissionAnnotation, "userType");
+//        Logical logical = (Logical) AnnotationUtils.getValue(permissionAnnotation, "logical");
+//        // 我把权限判断的逻辑封装到UserType枚举类中复用
+//        return UserType.hasPermission(userTypes, typeOfUser, logical);
+//    }
+//
+//}
