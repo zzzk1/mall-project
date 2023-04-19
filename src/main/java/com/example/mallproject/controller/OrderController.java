ae8ed6b1941adc3c261bdb43a71211d85864ff56
zzzk1 <36791902+zzzk1@users.noreply.github.com>
2023-04-19 14:21:15 +0800

    order 分页模糊查询

---
+++ src/main/java/com/example/mallproject/controller/OrderController.java
@@ -0,0 +1,35 @@
+package com.example.mallproject.controller;
+
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.mallproject.common.api.Result;
+import com.example.mallproject.entity.Order;
+import com.example.mallproject.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author zzzk1
+ * @since 2023-04-19
+ */
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("/page")
+    public Result<Page<Order>> getAll(@RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+                                      @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
+                                      @RequestParam(value = "username", defaultValue = "") String username) {
+        return Result.Success(orderService.selectAll(pageNum, pageSize, username));
+    }
+}
