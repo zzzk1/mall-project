41955fc56c77f7153ec7f4572eb251c9e9bb9669
zzzk1 <36791902+zzzk1@users.noreply.github.com>
2023-04-24 15:19:40 +0800

    文件上传 下载

--- src/main/java/com/example/mallproject/controller/FileController.java
+++ src/main/java/com/example/mallproject/controller/FileController.java
@@ -1,19 +1,15 @@
 package com.example.mallproject.controller;
 
 
-import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.util.IdUtil;
-import cn.hutool.core.util.StrUtil;
-import cn.hutool.crypto.SecureUtil;
 import com.example.mallproject.common.api.Result;
+import com.example.mallproject.common.utils.FileDBUtil;
 import com.example.mallproject.entity.FileDB;
 import com.example.mallproject.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
@@ -29,44 +25,22 @@ import java.util.List;
 @RequestMapping("/file")
 public class FileController {
 
-    @Value(value ="${files.upload.path}")
-    private String fileUploadPath;
-
     @Autowired
     private FileService fileService;
+    @Autowired
+    private FileDBUtil fileDBUtil;
     @PostMapping("/upload")
-    public Result<String> upload(@RequestParam MultipartFile file) throws IOException {
-        String originalFilename = file.getOriginalFilename();
-        String type = FileUtil.extName(originalFilename);
-        long size = file.getSize();
-
-        // 定义一个文件唯一的标识码
-        String uuid = IdUtil.fastSimpleUUID();
-        String fileUUID = uuid + StrUtil.DOT + type;
-
-        File uploadFile = new File(fileUploadPath + fileUUID);
-        // 判断配置的文件目录是否存在，若不存在则创建一个新的文件目录
-        File parentFile = uploadFile.getParentFile();
-        if(!parentFile.exists()) {
-            parentFile.mkdirs();
+    public Result<Boolean> upload(@RequestParam MultipartFile file) throws IOException {
+        Boolean result = fileDBUtil.upload(file);
+        if (result == false) {
+            return Result.Failed(result, "上传失败");
         }
+        return Result.Success(result, "上传成功");
+    }
 
-        String url;
-        // 获取文件的md5
-        String md5 = SecureUtil.md5(file.getInputStream());
-        // 从数据库查询是否存在相同的记录
-        FileDB dbFiles = fileService.getFileDB(md5);
-        if (dbFiles != null) { // 文件已存在
-            url = dbFiles.getUrl();
-            return Result.Failed(url, "文件已存在");
-        } else {
-            // 上传文件到磁盘
-            // 数据库若不存在重复文件，则不删除刚才上传的文件
-            url = "http://localhost:9090/file/" + fileUUID;
-        }
-        file.transferTo(uploadFile);
-        fileService.saveFileDB(originalFilename, type, size / 1024, url, md5);
-        return Result.Success(null, "上传成功");
+    @GetMapping("/{fileUUID}")
+    public void download(@PathVariable String fileUUID, HttpServletResponse response) throws IOException {
+        fileDBUtil.downLoad(fileUUID, response);
     }
 
     @DeleteMapping("{id}")
